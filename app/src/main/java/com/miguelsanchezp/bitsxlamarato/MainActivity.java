34f35d7b5e81a1dde8aa0bb248ec7969c048add2
miguelsanchezp <miguelsanchez1703@gmail.com>
2019-12-14 15:06:35 +0100

    Exportation of the location

--- app/src/main/java/com/miguelsanchezp/bitsxlamarato/MainActivity.java
+++ app/src/main/java/com/miguelsanchezp/bitsxlamarato/MainActivity.java
@@ -15,14 +15,19 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import static com.miguelsanchezp.bitsxlamarato.FileManipulation.writeDown;
 
 public class MainActivity extends AppCompatActivity {
     private FusedLocationProviderClient fusedLocationProviderClient;
     private double Latitude;
     private double Longitude;
     Button button;
+    Button push;
+    Button buttonExportLocation;
     private static final String TAG = "MainActivity";
     static String pathname;
+
+    private static final int REQUEST_POSITION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,25 +47,40 @@ public class MainActivity extends AppCompatActivity {
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    displayTheLastKnownLocation();
-                    establishSSHConnection();
+                    establishSSHConnectionRetriever(REQUEST_POSITION);
                 }
             });
-            pathname = this.getFilesDir().getAbsolutePath();
+            push = findViewById(R.id.buttonPush);
+            push.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    establishSSHConnectionPusher(REQUEST_POSITION);
+                }
+            });
+            buttonExportLocation = findViewById(R.id.buttonExport);
+            buttonExportLocation.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    exportLastKnownLocation();
+                }
+            });
+            pathname = this.getFilesDir().getAbsolutePath() + "/";
         }
     }
 
-    private void displayTheLastKnownLocation () {
-        Log.d(TAG, "displayTheLastKnownLocation: " + fusedLocationProviderClient.getLastLocation().toString());
+    private void exportLastKnownLocation() {
+        Log.d(TAG, "exportLastKnownLocation: " + fusedLocationProviderClient.getLastLocation().toString());
         fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
                 if (location != null) {
                     Latitude = location.getLatitude();
                     Longitude = location.getLongitude();
-                    String position = Latitude + ", " + Longitude;
+                    String position = Latitude + "%" + Longitude;
+                    writeDown(position, pathname + "positionPersonal.txt");
                     Log.d(TAG, "onSuccess: working");
-                    Toast.makeText(getApplicationContext(), position, Toast.LENGTH_LONG).show();
+                    establishSSHConnectionPusher(REQUEST_POSITION);
+                    Toast.makeText(getApplicationContext(), "Saved position successfully", Toast.LENGTH_LONG).show();
                 }else{
                     Toast.makeText(getApplicationContext(), "The location was null", Toast.LENGTH_LONG).show();
                 }
@@ -74,9 +94,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void establishSSHConnection () {
-         new ServerConnection().execute("start");
-         Toast.makeText(this, "begins the ssh connection", Toast.LENGTH_LONG).show();
+    private void establishSSHConnectionRetriever (int i) {
+         new DownloadFromServer().execute(i);
+         Toast.makeText(this, "begins the ssh retrieval", Toast.LENGTH_LONG).show();
+    }
 
+    private void establishSSHConnectionPusher (int i) {
+        new UploadToServer().execute(i);
+        Toast.makeText(this, "begins the ssh pushing", Toast.LENGTH_LONG).show();
     }
 }
